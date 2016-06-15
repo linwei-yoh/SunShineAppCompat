76f44bbd311aa341178682dbae494e4d3fd62ee9
linwei-yoh <395373035@qq.com>
2016-06-15 16:21:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+}
+
+
+
+
